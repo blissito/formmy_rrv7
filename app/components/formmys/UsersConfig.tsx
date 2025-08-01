525a30e9889fc920b524e69e9d665fa6d42c19a7
BrendaOrtega <bremin11.20.93@gmail.com>
2025-08-01 16:47:48 -0600

    users & notifications formmy flow is ready

---
+++ app/components/formmys/UsersConfig.tsx
@@ -0,0 +1,237 @@
+import { Card } from "~/components/chat/common/Card";
+import { Button } from "~/components/Button";
+import { FaUsers } from "react-icons/fa";
+import { twMerge } from "tailwind-merge";
+import {
+  Form,
+  useFetcher,
+  useNavigation,
+  useActionData,
+} from "react-router";
+import { type Permission, type User } from "@prisma/client";
+import { TrashIcon } from "~/components/icons/TrashIcon";
+import { InputModalForm } from "~/components/InputModalForm";
+import { useEffect, useState } from "react";
+import Spinner from "~/components/Spinner";
+import { Pluralize } from "~/components/Pluralize";
+import { Toggle } from "~/components/Switch";
+import { UsersTable } from "../chat/tab_sections/UsersTable";
+
+interface UsersConfigProps {
+  user: User;
+  permissions: Permission[];
+  projectName: string;
+  projectId: string;
+}
+
+export default function UsersConfig({ user, permissions, projectName, projectId }: UsersConfigProps) {
+  const navigation = useNavigation();
+  const fetcher = useFetcher();
+  const actionData = useActionData();
+  const [showModal, setShowModal] = useState(false);
+  
+  useEffect(() => {
+    if (
+      actionData &&
+      (actionData.success === true || actionData.success === false)
+    ) {
+      setShowModal(false);
+    }
+  }, [actionData]);
+
+  const isPlural = permissions.length > 1 || permissions.length === 0;
+  
+  return (
+    <section className="grid gap-5 relative">
+      <Card title="Usuarios">
+          <p className="text-xs text-gray-600 dark:text-gray-400 tracking-wide mb-8">
+            {permissions.length}
+            <Pluralize
+              singleWord="usuario"
+              isPlural={isPlural}
+              pluralWord="usuarios"
+            />
+            {" "}
+            <Pluralize
+              singleWord="agregado"
+              isPlural={isPlural}
+              pluralWord="agregados"
+            />
+          </p>
+          <Button
+            variant="primary"
+            onClick={() => setShowModal(true)}
+            className="flex items-center gap-2 absolute top-0 right-6"
+          >
+            <span>+ Usuario</span>
+          </Button>
+          <UsersTable
+                user={user}
+                permissions={permissions}
+                projectId={projectId}
+                isLoading={fetcher.state === "submitting"}
+              />
+
+    {showModal && (
+        <InputModalForm
+          isLoading={navigation.state !== "idle"}
+          onClose={() => setShowModal(false)}
+          title="Ingresa el correo del usuario"
+        />
+      )}
+      </Card>
+    </section>
+  );
+}
+
+const UserTable = ({
+  isLoading,
+  user,
+  permissions,
+  projectId,
+}: {
+  isLoading?: boolean;
+  permissions: Permission[];
+  user: User;
+  projectId: string;
+}) => {
+  return (
+    <section className="my-6 rounded-t-lg">
+      <Row className="bg-brand-300/20 dark:bg-transparent rounded-t-xl px-4 py-1">
+        <UserTableHeader />
+      </Row>
+
+      <Row>
+        <UserInfo isOwner user={user} />
+      </Row>
+
+      {permissions.map((p) => (
+        <Row key={p.email}>
+          <UserInfo permission={p} isLoading={isLoading} projectId={projectId} />
+        </Row>
+      ))}
+    </section>
+  );
+};
+
+const UserInfo = ({
+  isLoading,
+  permission,
+  isOwner = false,
+  user,
+  projectId,
+}: {
+  user?: User;
+  isLoading?: boolean;
+  isOwner?: boolean;
+  permission: Permission | User;
+  projectId: string;
+}) => {
+  const fetcher = useFetcher();
+  
+  const handleToggleNotifications = (value: boolean) => {
+    fetcher.submit(
+      {
+        intent: "toggle_notifications_for_permission",
+        permissionId: permission.id,
+        value,
+      },
+      { method: "POST", action: "/api/formmy" }
+    );
+  };
+
+  const avatar =
+    "https://secure.gravatar.com/avatar/23709cd232fbb194583b2af3fe6889dd?s=256&d=mm&r=g";
+    
+  return (
+    <>
+      <div className="flex col-span-3 items-center gap-2">
+        <img
+          onError={(ev) => {
+            ev.target.onerror = null;
+            ev.target.src = avatar;
+          }}
+          className="w-[30px] h-[30px] rounded-full object-cover m-1"
+          src={
+            (isOwner && user ? user.picture : permission?.user?.picture) ??
+            avatar
+          }
+          alt={isOwner && user ? user.name : permission?.user?.name ?? "avatar"}
+        />
+        <p className="flex flex-col">
+          <span className="font-bold text-xs text-gray-600 dark:text-white">
+            {isOwner ? user?.name : permission.user?.name}
+          </span>
+          <span className="text-xs text-gray-500 dark:text-gray-400 font-light">
+            {isOwner && user ? user.email : permission?.email}
+          </span>
+        </p>
+      </div>
+      <p className="font-light">{isOwner ? "Propietario" : "Invitado"}</p>
+      <p
+        className={twMerge(
+          "font-light text-[#66B86C] dark:text-[#37E8A3] capitalize",
+          permission?.status === "pending" &&
+            "text-yellow-500 dark:text-yellow-500",
+          permission?.status === "rejected" &&
+            "text-[#ED695F] dark:text-[#EB5757]"
+        )}
+      >
+        {isOwner ? "Active" : permission?.status}
+      </p>
+      <fetcher.Form
+        method="post"
+        className={twMerge("flex gap-3 items-center", isOwner && "hidden")}
+      >
+        <input
+          className="hidden"
+          name="permissionId"
+          defaultValue={permission?.id}
+        />
+        <input type="hidden" name="intent" value="delete_permission" />
+        <button
+          type="submit"
+          className="group-hover:visible invisible"
+        >
+          {isLoading ? <Spinner /> : <TrashIcon fill="tomato" />}
+        </button>
+        {permission?.status === "active" && (
+          <Toggle
+            onChange={handleToggleNotifications}
+            defaultValue={permission?.notifications}
+          />
+        )}
+      </fetcher.Form>
+    </>
+  );
+};
+
+const UserTableHeader = () => {
+  const classs = "text-space-800 dark:text-white text-xs font-bold py-1";
+  return (
+    <>
+      <p className={twMerge(classs, "col-span-3")}>Usuario</p>
+      <p className={classs}>Role</p>
+      <p className={classs}>Estatus</p>
+      <p className={classs}>Notificaciones</p>
+    </>
+  );
+};
+
+const Row = ({
+  className,
+  ...props
+}: {
+  className?: string;
+  [x: string]: any;
+}) => {
+  return (
+    <div
+      className={twMerge(
+        "grid grid-cols-6 px-4 py-3 gap-2 border-b border-b-indigo-100 dark:border-b-white/10 text-xs items-center group",
+        className
+      )}
+      {...props}
+    />
+  );
+};
--- app/routes/dashboard.formmys_.$projectId_.settings.tsx
+++ app/routes/dashboard.formmys_.$projectId_.settings.tsx
@@ -1,8 +1,5 @@
-import { data as json } from "react-router";
+import { data as json, redirect } from "react-router";
 import { useFetcher, useLoaderData } from "react-router";
-import { ProTag } from "~/components/ProTag";
-import Spinner from "~/components/Spinner";
-import { Toggle } from "~/components/Switch";
 import { db } from "~/utils/db.server";
 import { getUserOrNull } from "server/getUserUtils.server";
 import { type Notifications, notificationsSchema } from "~/utils/zod";
@@ -10,27 +7,64 @@ import type { Route } from "./+types/dash_.$projectId_.settings.notifications";
 import { PageContainer, StickyGrid } from "~/components/chat/PageContainer";
 import { ConfigMenu, GeneralButton, NotificacionesButton, UsuariosButton } from "~/components/chat/ConfigMenu";
 import { Card } from "~/components/chat/common/Card";
-import { Toggler } from "~/components/chat/tab_sections/Configuracion";
-import { UsersTable } from "~/components/chat/tab_sections/UsersTable";
 import { useChipTabs } from "~/components/chat/common/ChipTabs";
 import { useState } from "react";
 import { NotificationsConfig } from "~/components/formmys/NotificationsConfig";
+import { GeneralConfig } from "~/components/formmys/GeneralConfig";
+import UsersConfig from "~/components/formmys/UsersConfig";
+import { UsersTable } from "~/components/chat/tab_sections/UsersTable";
 
 export const action = async ({ request, params }: Route.ActionArgs) => {
+  const user = await getUserOrNull(request);
   const formData = await request.formData();
+  const intent = formData.get("intent");
+  
+  if (intent === "delete") {
+    // @TODO: delete files from firebase
+    await db.answer.deleteMany({ where: { projectId: params.projectId } }); // cascade deleting
+    await db.project.delete({ where: { id: params.projectId } });
+    return redirect("/dashboard/formmys");
+  }
+  
+  // @TODO: validate permission is owner? ??
+  if (intent === "send_invite") {
+    const email = (formData.get("email") as string).toLowerCase();
+    if (!email || email === user?.email) return { success: false };
+
+    const permissionData = {
+      email: String(email),
+      can: { read: true },
+      projectId: String(params.projectId), // @TODO: check if exists
+    };
+    const exists = await db.permission.findFirst({
+      where: { email, projectId: params.projectId },
+    });
+    if (exists) return json({ success: true }, { status: 200 }); // retun null equivalent
+    // if user add it
+    const userExists = await db.user.findUnique({ where: { email } });
+    if (userExists) {
+      // @ts-ignore
+      permissionData.userId = userExists.id;
+    }
+    await db.permission.create({ data: permissionData });
+    const project = await db.project.findUnique({
+      where: { id: params.projectId },
+    });
+    //@ts-ignore
+    // sendInvite({ project, email }); // Comentado por ahora si no tienes la función
+    return { success: true };
+  }
+
+  if (intent === "delete_permission") {
+    const permissionId = formData.get("permissionId") as string;
+    if (!permissionId) return json(null, { status: 404 });
+
+    await db.permission.delete({ where: { id: permissionId } });
+    return null;
+  }
+  
   const notifications = Object.fromEntries(formData);
   const validData = notificationsSchema.parse(notifications) as Notifications;
- 
-
-  const copyToClipboard = async (text: string) => {
-    try {
-      await navigator.clipboard.writeText(text);
-      setIsCopied(true);
-      setTimeout(() => setIsCopied(false), 2000);
-    } catch (err) {
-      console.error("Failed to copy text: ", err);
-    }
-  };
 
   await db.project.update({
     where: { id: params.projectId },
@@ -40,17 +74,26 @@ export const action = async ({ request, params }: Route.ActionArgs) => {
   return null;
 };
 
-// @TODO we need to know if user is prom in this route
 export const loader = async ({ params, request }: Route.LoaderArgs) => {
   const user = await getUserOrNull(request); // why? well, not sure... just don't want to redirect.
   const project = await db.project.findUnique({
     where: { id: params.projectId },
+    include: {
+      permissions: {
+        include: {
+          user: true,
+        },
+      },
+    },
   });
   if (!project) throw json(null, { status: 404 });
   const isPro = user?.plan === "PRO" ? true : false;
   return {
     isPro,
     notifications: project.settings?.notifications,
+    project,
+    user,
+    permissions: project.permissions,
   };
 };
 
@@ -58,6 +101,9 @@ export default function Route() {
   const {
     notifications = { new: false, members: false, warning: false }, // true defaults ;)
     isPro,
+    project,
+    user,
+    permissions,
   } = useLoaderData<typeof loader>();
   // let settings = {};
   const fetcher = useFetcher();
@@ -79,8 +125,10 @@ export default function Route() {
     fetcher.submit({ ...notifications, new: bool }, { method: "post" });
   };
 
-  const { currentTab, setCurrentTab } = useChipTabs("seguridad");
+  const { currentTab, setCurrentTab } = useChipTabs("general");
   const [isCopied, setIsCopied] = useState(false);
+  const [showConfirm, setShowConfirm] = useState(false);
+  const [match, set] = useState("");
 
   const copyToClipboard = async (text: string) => {
     try {
@@ -90,6 +138,10 @@ export default function Route() {
     } catch (err) {
       console.error("Failed to copy text: ", err);
     }
+  };
+  const handleDelete = () => {
+    fetcher.submit({ intent: "delete" }, { method: "post" });
+    // toast({ text: "Tu formmy se ha eliminado correctamente 🥺" });
   };
   return (
     <PageContainer>
@@ -117,70 +169,17 @@ export default function Route() {
             </ConfigMenu>
       
             {currentTab === "general" && (
-              <section className="grid gap-5">
-                <Card title="General">
-                  <div className="mb-6 mt-4">
-                    <span className="text-sm text-gray-600 block mb-2">
-                      Id de tu Formmy
-                    </span>
-                    <nav className="flex gap-2 items-center">
-                {/* <p className="font-mono text-sm">{project.id}</p> */}
-                {/* <button
-                  onClick={() => copyToClipboard(project.id)}
-                  className="w-6 h-6 p-1 rounded-lg hover:bg-gray-100 border border-gray-300 flex items-center justify-center transition-colors"
-                  aria-label="Copiar ID"
-                  title="Copiar al portapapeles"
-                >
-                  {isCopied ? (
-                    <svg
-                      className="w-3.5 h-3.5 text-green-500"
-                      fill="none"
-                      viewBox="0 0 24 24"
-                      stroke="currentColor"
-                    >
-                      <path
-                        strokeLinecap="round"
-                        strokeLinejoin="round"
-                        strokeWidth={2}
-                        d="M5 13l4 4L19 7"
-                      />
-                    </svg>
-                  ) : (
-                    <img
-                      src="/assets/chat/copy.svg"
-                      alt="Copiar"
-                      className="w-3.5 h-3.5"
-                    />
-                  )}
-                </button> */}
-              </nav>
-                  </div>
-                  <div className="mb-6">
-                    <span className="text-sm text-gray-600 block mb-2">Tamaño</span>
-                  
-                  </div>
-                  <div className="mb-0">
-                    <span className="text-sm text-gray-600 block mb-2">
-                      Historial del chat
-                    </span>
-                    <p>7 días</p>
-                  </div>
-                </Card>
-                <Card
-                  title="Eliminar chatbot"
-          
-                >
-                 <div className="flex gap-6">
-                  <p className="text-metal max-w-[700px]">Una vez que elimines tu chatbot, tu agente será eliminado al igual que toda la información que subiste. Está acción es irreversible, así que asegúrate de que está es la acción que quieres tomar.</p>
-                  <button
-                    disabled
-                    className="block max-w-[220px] ml-auto disabled:opacity-50 disabled:cursor-not-allowed w-full bg-red-500 text-white py-2 px-4 rounded-full"
-                  >
-                    Eliminar
-                  </button>
-                 </div>        
-                </Card>
-              </section>
+              <GeneralConfig
+                project={project}
+                isCopied={isCopied}
+                showConfirm={showConfirm}
+                match={match}
+                fetcherState={fetcher.state}
+                onCopyToClipboard={copyToClipboard}
+                onSetShowConfirm={setShowConfirm}
+                onSetMatch={set}
+                onHandleDelete={handleDelete}
+              />
             )}
       
             {currentTab === "notificaciones" && (
@@ -194,11 +193,15 @@ export default function Route() {
             )}
       
             {currentTab === "usuarios" && (
-              <section className="">
-                <Card title="Administra usuarios" text="3 usuarios">
-                  <UsersTable />
-                </Card>
-              </section>
+              <>
+              <UsersConfig
+                user={user}
+                permissions={permissions}
+                projectName={project.name}
+                projectId={project.id}
+              />
+             
+              </>
             )}
       
        
@@ -207,4 +210,3 @@ export default function Route() {
     </PageContainer>
   );
 }
-
