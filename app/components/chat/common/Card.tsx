1f44c7364b82a7c947c96655fcd67e21572baa90
BrendaOrtega <bremin11.20.93@gmail.com>
2025-08-07 15:15:08 -0600

    404 & ui fixes

--- app/components/chat/common/Card.tsx
+++ app/components/chat/common/Card.tsx
@@ -198,7 +198,7 @@ export const IntegrationCard = ({
       <nav className="flex gap-2">
         <SimpleButton
           className={`grow ${
-            isConnected ? "text-success border-success bg-success/10" : "text-metal"
+            isConnected ? "text-success border-success bg-success/20 hover:bg-success/30" : "text-metal"
           }`}
           onClick={getButtonAction()}
         >
